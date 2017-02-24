2899085106ecc5c6d989704776da9182d1c9e0cf
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2017-02-24 16:31:03 +0700

    A

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use View;
+class PagesController extends Controller
+{
+    public function index(){
+    	if(View::exists('pages.index')){
+    		return view('pages.index',['text'=>'<b>Laravel<b/>']);
+    	}
+    	else return 'No view available';
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+Hello from index in pages folder.
+
+<p>{!! $text !!}</p>
+
+<p>{{$text}}<p/>
+
